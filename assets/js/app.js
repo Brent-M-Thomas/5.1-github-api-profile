5ffcd1461c9f476e50858819e29ea1526050fc27
Brent Thomas <brentmt24@gmail.com>
2015-07-27 15:55:25 -0500

    working on html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,23 @@
 (function() {
 
+$.ajax({
+  url: 'https://api.github.com/users/Brent-M-Thomas',
+  dataType: 'json',
+  success:
+  error: function() {
+    alert('Whatever went wrong must be your fault because it works on my machine!'),
+  }
+$.ajax({
+  url: 'https://api.github.com/users/Brent-M-Thomas/repos',
+  dataType: 'json',
+  success:
+  error: function() {
+    alert('Whatever went wrong must be your fault because it works on my machine!'),
+  }
+
+
+});
+
+
 
 })();
