312e7cc305660f3b9aabc41ef7fe3c2be87c0963
Brent Thomas <brentmt24@gmail.com>
2015-07-28 08:51:29 -0500

    both apis pulled in. working on styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,41 @@
 (function() {
+  var newTemplate = function(querySelector) {
+    var el = $(querySelector);
+    return Handlebars.compile(el.html());
+  };
 
-$.ajax({
-  url: 'https://api.github.com/users/Brent-M-Thomas',
-  dataType: 'json',
-  success:
-  error: function() {
-    alert('Whatever went wrong must be your fault because it works on my machine!'),
-  }
-$.ajax({
-  url: 'https://api.github.com/users/Brent-M-Thomas/repos',
-  dataType: 'json',
-  success:
-  error: function() {
-    alert('Whatever went wrong must be your fault because it works on my machine!'),
-  }
+  $.ajax({
+    url: 'https://api.github.com/users/Brent-M-Thomas',
+    dataType: 'json',
+    success: function(loadData) {
+      var t = newTemplate('.profile-data');
+      $('.left-side').append(t(loadData));
+    },
 
+    error: function() {
+      alert('Whatever went wrong must be your fault because it works on my machine!');
+    }
+  });
 
-});
+  $.ajax({
+    url: 'https://api.github.com/users/Brent-M-Thomas/repos',
+    dataType: 'json',
+    success: function(loadData) {
+      var t = newTemplate('.content');
+      loadData.forEach(function(data) {
+        $('.content-main').append(t(data));
 
+      });
+    },
 
+    // beerData.data.forEach(function(beer) {
+    // var addBeer = template(beer);
+
+    // targetEl.innerHTML += addBeer;
+
+    error: function() {
+      alert('Whatever went wrong must be your fault because it works on my machine!');
+    }
+  });
 
 })();
